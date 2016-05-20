f51a3a20fe05bf9568ff204f47259ab4b6d78c54
Christian Parker <christian.parker@emc.com>
2016-05-19 18:55:21 -0600

    misc changes

--- arduino/car-mon/car-mon.ino
+++ arduino/car-mon/car-mon.ino
@@ -6,9 +6,26 @@
 
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
+#include <EEPROM.h>
+
 
 // when we acquire a location from GPS, this is the maximum # of attempts per loop()
 const int maxGPSReadAttempts = 25;
+
+const int builtinRedLED = 16;
+
+const int gpsPowerPin = 2;
+const int sim900PowerPin = 0;
+
+// if speed in MPH is faster than this, we're moving
+const int movingThresholdMPH = 10;
+
+// if we're still for this many cycles, go into deep sleep
+const int numStillCyclesForDeepSleep = 10;
+
+// if the truck is moving , post updates more often
+const int movingSleepIntervalSec = 30;
+const int stationarySleepIntervalSec = 5 * 60;
 
 const String postURL = "http://truck-monitor.herokuapp.com/truckLocation";
 const int jsonPostSize = 256;
@@ -19,43 +36,156 @@ int sim900Baud = 19200;
 int gpsBaud = 9600;
 
 // pins for software serial communication with GPS
-int gpsTXPin = 0;
-int gpsRXPin = 2;
+int gpsTXPin = 5;
+int gpsRXPin = 4;
 
 // SIM900x
 int sim900TXPin = 14;
 int sim900RXPin = 12;
 
-TinyGPSPlus gps;
+// for keeping count of how long we're sitting still
+byte stillCycleCount = 0;
 
-SoftwareSerial gpsSerial(gpsRXPin, gpsTXPin,false,1024);
-SoftwareSerial sim900Serial(sim900RXPin, sim900TXPin,false,64);
+TinyGPSPlus gps;
+SoftwareSerial gpsSerial(gpsRXPin, gpsTXPin,false,1024);;
+SoftwareSerial sim900Serial(sim900RXPin, sim900TXPin,false,64);;
 
 
 void setup() {
+  EEPROM.begin(4);
+
+  pinMode(builtinRedLED, OUTPUT);
+  digitalWrite(builtinRedLED, LOW);
+
+  // power up the GPS
+  digitalWrite(gpsPowerPin, LOW);
+
+  // everything is in setup because we're using deep sleep
   Serial.begin(ioSerialBaud);
-  Serial.println("Hello");
+  Serial.println("Truck Monitor - cp@cjparker.us 2016");
+
+  stillCycleCount = EEPROM.read(0);
+  if (stillCycleCount > 100) {
+    // this means we're uninitialized
+    Serial.println("setting eeprom value for the first time");
+    EEPROM.write(0,0);
+  }
 
   gpsSerial.begin(gpsBaud);
   sim900Serial.begin(sim900Baud);
+  Serial.flush();
+  gpsSerial.flush();
+  sim900Serial.flush();
+  delay(100);
+
+  // check status of, and possibly power on sim900
+  checkSim900Status();
+
+  gpsSerial.enableRx(true);
+  sim900Serial.enableRx(false);
+
+  boolean postedLocation = false;
+
+  while (!postedLocation) {
+    getLocationFromGPS();
+
+    if (gps.location.isValid()) {
+      gpsSerial.enableRx(false);
+      sim900Serial.enableRx(true);
+      String jsonString = buildGPSJSON();
+      Serial.println(jsonString);
+      postJSON(jsonString);
+      postedLocation = true;
+    } else {
+      Serial.print("Acquiring fix.  # of sattelites ");
+      Serial.println(gps.satellites.value());
+    }
+    digitalWrite(builtinRedLED, !digitalRead(builtinRedLED));
+    delay(1000);
+  }
+
+  // turn off red led
+  digitalWrite(builtinRedLED, HIGH);
+
+  // power down the GPS
+  digitalWrite(gpsPowerPin, HIGH);
+
+  if (gps.speed.mph() >= movingThresholdMPH) {
+    Serial.println("we're moving");
+    stillCycleCount = 0;
+    EEPROM.write(0,stillCycleCount);
+    EEPROM.commit();
+  } else {
+    Serial.println("we're stationary");
+    stillCycleCount++;
+    if (stillCycleCount <= 100) {
+      EEPROM.write(0,stillCycleCount);
+      EEPROM.commit();
+    }
+  }
+
+  // long or short nap
+  if (stillCycleCount >= numStillCyclesForDeepSleep) {
+    Serial.println("deep sleep");
+    ESP.deepSleep(stationarySleepIntervalSec * 1000000);
+  } else {
+    Serial.println("short sleep");
+    ESP.deepSleep(movingSleepIntervalSec * 1000000);
+  }
 }
+
+
+void checkSim900Status() {
+  char buffer[64];
+  int count = 0;
+  gpsSerial.enableRx(false);
+  sim900Serial.enableRx(true);
+
+  sim900Serial.println("AT");
+  delay(100);
+  while(sim900Serial.available() > 0) {
+    buffer[count++] = sim900Serial.read();
+    if (count == 64) {
+      break;
+    }
+  }
+
+  // if the buffer contains 'OK' , we're good
+  if (String(buffer).indexOf('OK') != -1) {
+    Serial.println("sim900 is active");
+  } else {
+    Serial.println("sim900 is not responding, powering on");
+  }
+
+  digitalWrite(sim900PowerPin, HIGH);
+  delay(1000);
+  digitalWrite(sim900PowerPin, LOW);
+
+  // wait for it to start, then try to talk to it again
+  delay(5000);
+  sim900Serial.println("AT");
+  delay(100);
+  count = 0;
+  while(sim900Serial.available() > 0) {
+    buffer[count++] = sim900Serial.read();
+    if (count == 64) {
+      break;
+    }
+  }
+
+  if (String(buffer).indexOf('OK') != -1) {
+    Serial.println("sim900 is active");
+  } else {
+    Serial.println("sim900 STILL NOT RESPONDING");
+  }
+}
+
 
 unsigned char buffer[64]; // buffer array for data recieve over serial port
 int count = 0;   // counter for buffer array
 
 void loop()
 {
-  getLocationFromGPS();
-
-  if (gps.location.isValid()) {
-    String jsonString = buildGPSJSON();
-    Serial.println(jsonString);
-    postJSON(jsonString);
-  } else {
-    Serial.print("Acquiring fix.  # of sattelites ");
-    Serial.println(gps.satellites.value());
-  }
-  delay(30 * 1000);
 }
 
 
@@ -80,34 +210,6 @@ void getLocationFromGPS() {
 
 
 
-void postUpdate() {
-  // number of times to wait for a complete set of data from GPS module
-  boolean successfulRead = false;
-
-  gpsSerial.flush();
-  for (int i = 0; i < maxGPSReadAttempts && !successfulRead; i++) {
-    Serial.print("pass ");
-    Serial.println(i);
-    while (gpsSerial.available() > 0) {
-      Serial.println("we have avail");
-      if (gps.encode(gpsSerial.read())) {
-        if (gps.location.isValid()) {
-          Serial.println("gps location is valid");
-          String gpsJSON = "";
-          Serial.print("GPS JSON ");
-          Serial.println(gpsJSON);
-          postJSON(gpsJSON);
-          successfulRead = true;
-        } else {
-          Serial.println("location wasn't valid");
-          delay(1000);
-        }
-      }
-    }
-  }
-
-
-}
 
 /*
  return a JSON string of gps data
@@ -187,11 +289,15 @@ void postJSON(String json) {
   Serial.println("done sending json");
 }
 
+
+
 void copySIMResponseToSerialIO() {
   while(sim900Serial.available()) {
     Serial.write(sim900Serial.read());
   }
 }
+
+
 
 void sendSIMCommands(String commands[]) {
   int commandDelay = 2000;
@@ -200,6 +306,7 @@ void sendSIMCommands(String commands[]) {
   boolean done = false;
   int i = 0;
   while (!done) {
+    digitalWrite(builtinRedLED, !digitalRead(builtinRedLED));
     String cmd = commands[i];
     if (String(cmd) != String(doneStr)) {
       Serial.print("sending command: ");
@@ -213,71 +320,5 @@ void sendSIMCommands(String commands[]) {
       done = true;
     }
     i++;
-  }
-}
-
-
-
-void displayGPSInfo()
-{
-  Serial.print(F("Location: "));
-  if (gps.location.isValid())
-  {
-    Serial.print(gps.location.lat(), 6);
-    Serial.print(F(","));
-    Serial.print(gps.location.lng(), 6);
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F("  Date/Time: "));
-  if (gps.date.isValid())
-  {
-    Serial.print(gps.date.month());
-    Serial.print(F("/"));
-    Serial.print(gps.date.day());
-    Serial.print(F("/"));
-    Serial.print(gps.date.year());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.print(F(" "));
-  if (gps.time.isValid())
-  {
-    if (gps.time.hour() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.hour());
-    Serial.print(F(":"));
-    if (gps.time.minute() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.minute());
-    Serial.print(F(":"));
-    if (gps.time.second() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.second());
-    Serial.print(F("."));
-    if (gps.time.centisecond() < 10) Serial.print(F("0"));
-    Serial.print(gps.time.centisecond());
-  }
-  else
-  {
-    Serial.print(F("INVALID"));
-  }
-
-  Serial.println("");
-  Serial.print("Altitude Feet: ");  Serial.println(gps.altitude.feet());
-  Serial.println("Speed MPH"); Serial.println(gps.speed.mph());
-
-  Serial.println();
-}
-
-
-void clearBufferArray()              // function to clear buffer array
-{
-  for (int i = 0; i < count; i++)
-  {
-    buffer[i] = NULL; // clear all index of array with command NULL
   }
 }
--- main.js
+++ main.js
@@ -8,13 +8,13 @@ app.config(function(uiGmapGoogleMapApiProvider) {
 
 });
 
-app.factory('dataService', ['$q','$http', function($q,$http) {
+app.factory('dataService', ['$q', '$http', function($q, $http) {
 
     var currentLocationURL = '/truckLastLocation';
     var locationHistoryURL = '/truckLocation';
 
     var currentLocation = function() {
-      return $http.get(currentLocationURL);
+        return $http.get(currentLocationURL);
 
         /*
         return $q.when({
@@ -28,41 +28,41 @@ app.factory('dataService', ['$q','$http', function($q,$http) {
     };
 
     var locationHistory = function() {
-      return $http.get(locationHistoryURL);
+        return $http.get(locationHistoryURL);
 
-      /*
-        return $q.when([{
-            "lat": 39.9787232,
-            "lon": -105.161233,
-            "speedMPH": 20.0,
-            "altFt": 12.0,
-            "heading": 123.0
-        }, {
-            "lat": 40.3487232,
-            "lon": -106.331233,
-            "speedMPH": 20.0,
-            "altFt": 12.0,
-            "heading": 123.0
-        }, {
-            "lat": 41.1187232,
-            "lon": -107.561233,
-            "speedMPH": 20.0,
-            "altFt": 12.0,
-            "heading": 123.0
-        }, {
-            "lat": 42.3787232,
-            "lon": -108.561233,
-            "speedMPH": 20.0,
-            "altFt": 12.0,
-            "heading": 123.0
-        }, {
-            "lat": 43.3787232,
-            "lon": -109.561233,
-            "speedMPH": 20.0,
-            "altFt": 12.0,
-            "heading": 123.0
-        }]);
-        */
+        /*
+          return $q.when([{
+              "lat": 39.9787232,
+              "lon": -105.161233,
+              "speedMPH": 20.0,
+              "altFt": 12.0,
+              "heading": 123.0
+          }, {
+              "lat": 40.3487232,
+              "lon": -106.331233,
+              "speedMPH": 20.0,
+              "altFt": 12.0,
+              "heading": 123.0
+          }, {
+              "lat": 41.1187232,
+              "lon": -107.561233,
+              "speedMPH": 20.0,
+              "altFt": 12.0,
+              "heading": 123.0
+          }, {
+              "lat": 42.3787232,
+              "lon": -108.561233,
+              "speedMPH": 20.0,
+              "altFt": 12.0,
+              "heading": 123.0
+          }, {
+              "lat": 43.3787232,
+              "lon": -109.561233,
+              "speedMPH": 20.0,
+              "altFt": 12.0,
+              "heading": 123.0
+          }]);
+          */
     };
 
     return {
@@ -74,34 +74,39 @@ app.factory('dataService', ['$q','$http', function($q,$http) {
 
 app.controller('AppCtrl', ['$scope', '$interval', 'dataService', 'uiGmapGoogleMapApi',
     function($scope, $interval, dataService, uiGmapGoogleMapApi) {
-        $interval(function() {
-            $scope.now = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
-        }, 1000);
+        var pollInterval = 5000;
 
         $scope.polylines = [];
 
-        dataService.currentLocation()
-            .then(function(cur) {
-                $scope.map.center = {
-                  latitude: cur.data.lat,
-                  longitude: cur.data.lon
-                };
-
-                $scope.current = {
-                    altFt: cur.data.altFt,
-                    locationDetails:cur.data.locationDetails,
-                    speedMPH: cur.data.speedMPH,
-                    heading: cur.data.heading,
-                    coords: {
+        var updateCurrentLocation = function() {
+            dataService.currentLocation()
+                .then(function(cur) {
+                    $scope.map.center = {
                         latitude: cur.data.lat,
                         longitude: cur.data.lon
-                    },
-                    options: {
-                        clickable: false,
-                        icon: 'favicon-32x32.png'
-                    }
-                };
-            });
+                    };
+
+                    $scope.current = {
+                        lastUpdate: moment(cur.data.dateTime).fromNow(),
+                        altFt: cur.data.altFt,
+                        locationDetails: cur.data.locationDetails,
+                        speedMPH: cur.data.speedMPH,
+                        heading: cur.data.heading,
+                        coords: {
+                            latitude: cur.data.lat,
+                            longitude: cur.data.lon
+                        },
+                        options: {
+                            clickable: false,
+                            icon: 'favicon-32x32.png'
+                        }
+                    };
+                });
+        };
+
+        $interval(function() {
+          updateCurrentLocation();
+        }, pollInterval);
 
         dataService.locationHistory()
             .then(function(historyPoints) {
@@ -136,5 +141,7 @@ app.controller('AppCtrl', ['$scope', '$interval', 'dataService', 'uiGmapGoogleMa
                 streetViewControl: true
             }
         };
+
+        updateCurrentLocation();
     }
 ]);
